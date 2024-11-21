cc51a55e8c4c6601c430daf5520213b19f5bdd73
BrentWeiffenbach <rweiff2022@gmail.com>
2024-11-21 15:06:53 -0500

    added njit functions for opimizations Co-authored-by: Alexander Beck <AlexanderBeck0@users.noreply.github.com>

--- raycasting.py
+++ raycasting.py
@@ -1,7 +1,26 @@
 import numpy as np
 import numpy.typing as npt
 from typing import Annotated, Optional, List
+from numba import njit
+from numba.np.extensions import cross2d
 
+@njit
+def ray_math(v1, v2, v3):
+    v1 = v1.astype(np.float64)
+    v2 = v2.astype(np.float64)
+    v3 = v3.astype(np.float64)
+    dot = np.dot(v2, v3)
+    if abs(dot) < 1e-6:
+        return
+    t1 = cross2d(v2, v1) / dot
+    t2 = np.dot(v1, v3) / dot
+    # if t1 >= 0 and 0 <= t2 <= 1:
+    #     return ray_origin + t1 * ray_direction
+    # return None
+    if t1 >= 0 and 0 <= t2 <= 1:
+        return t1
+
+@njit
 def intersect(ray_origin: np.ndarray, ray_direction: np.ndarray, segment_start: np.ndarray, segment_end: np.ndarray) -> Optional[np.ndarray]:
     """
     Calculates intersection between a ray and polygon segment
@@ -15,22 +34,16 @@ def intersect(ray_origin: np.ndarray, ray_direction: np.ndarray, segment_start: 
     Returns:
         Optional[np.ndarray]: Intersection point if ray intersects segment
     """
-    segment_start = np.array(segment_start)
-    segment_end = np.array(segment_end)
+    # segment_start = np.array(segment_start)
+    # segment_end = np.array(segment_end)
     v1 = ray_origin - segment_start
     v2 = segment_end - segment_start
     # perpendicular vector to the ray's direction
-    v3 = np.array([-ray_direction[1], ray_direction[0]])
+    v3 = np.array([-ray_direction[1], ray_direction[0]], dtype=np.float64)
+
+    ray_math_result = ray_math(v1, v2, v3)
     
-    dot = np.dot(v2, v3)
-    if abs(dot) < 1e-6:
-        return None
-    t1 = np.cross(v2, v1) / dot
-    t2 = np.dot(v1, v3) / dot
-    
-    if t1 >= 0 and 0 <= t2 <= 1:
-        return ray_origin + t1 * ray_direction
-    return None
+    return  ray_origin + ray_math_result * ray_direction if ray_math_result is not None else None
 
 def find_last_intersection(center: Annotated[npt.NDArray[np.int32], (2,)], direction_pos: Annotated[npt.NDArray[np.float32], (2,)], polygon_points: npt.NDArray[np.int32]) -> npt.NDArray[np.float32]:
     """
@@ -38,7 +51,7 @@ def find_last_intersection(center: Annotated[npt.NDArray[np.int32], (2,)], direc
     
     Args:
         center (np.ndarray): center of mass for polygon
-        direction_pos (np.ndarray): currently mouse click position eventually just a direction
+        direction_pos (np.ndarray): direction of vector towards gripper pose
         polygon_points (np.ndarray): list of polygon vertices as a 2d array
     
     Returns:
@@ -50,7 +63,7 @@ def find_last_intersection(center: Annotated[npt.NDArray[np.int32], (2,)], direc
     for i in range(len(polygon_points)):
         segment_start = polygon_points[i]
         segment_end = polygon_points[(i+1) % len(polygon_points)]
-        intersection = intersect(center, ray_direction, segment_start, segment_end)
+        intersection = intersect(center, ray_direction, np.array(segment_start), np.array(segment_end))
         if intersection is not None:
             intersections.append(intersection)
     
